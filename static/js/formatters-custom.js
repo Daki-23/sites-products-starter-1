38753fb584fb0c7ed1e97d227a1b64ef19a4c9ba
cdemetriou <cdemetriou@yext.com>
2023-05-03 20:30:58 +0000

    tryrth

--- static/js/formatters-custom.js
+++ static/js/formatters-custom.js
@@ -1,7 +1,7 @@
 /**
  * The formatters-custom file is a place to define custom formatters or to
  * override any of the baked-in formatters like phone number formatting.
- *
+ * hello
  * e.g. You may write something like below
  * export function address(profile) {
  *   return `111 Yext Street`;
